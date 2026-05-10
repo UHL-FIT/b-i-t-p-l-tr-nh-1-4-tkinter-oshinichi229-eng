1396d7f4c0a71219d82418b64a7a561ba78236ce
oshinichi229-eng <oshinichi229@gmail.com>
2026-05-11 00:52:40 +0700

    Create a simple Tkinter GUI application
    Initialize a basic Tkinter application with a window and a label.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello world!')
+import tkinter as tk
+#1.khoi tao cua so goc
+root=tk.Tk()
+root.title("ung dung dau tien-UHL")
+root.geometry("500x500")
+#2.tao thanh phan hien thi van ban (label)
+nhan_chao=tk.Label(root,text="nguyen thi bich ngoc")
+nhan_chao.pack(pady=50)#dua nhan vao cua so tao khoang cach le
+#duy tri cua so (vong lap chinh)
+root.mainloop()
